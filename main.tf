4301e7e12b83dbb59dbf226c9ac5bbf5a786c0f2
Peter Mascaro <peter.mascaro@au.abb.com>
2016-08-02 14:29:17 +1000

    Add source code comments

--- main.tf
+++ main.tf
@@ -1,47 +1,59 @@
-# amazon linux 2016.03.3
+# AMI region hash for Amazon Linux 2016.03.3
 variable "aws_amis" {
   "default" = {
     "ap-southeast-2" = "ami-dc361ebf"
   }
 }
 
+# Default user for ssh connection
 variable "aws_instance_connection_user" {
   "default" = "ec2-user"
 }
 
+# Default region for aws provider
 variable "aws_region" {
   "default" = "ap-southeast-2"
 }
 
+# Default source cidr for ssh connection
 variable "ssh_source_cidr_block" {
   "default" = "0.0.0.0/0"
 }
 
+# Default source cidr for web (nginx/kibana) requests
 variable "web_source_cidr_block" {
   "default" = "0.0.0.0/0"
 }
 
+# Default vpc cidr block
 variable "aws_vpc_cidr_block" {
   "default" = "10.0.0.0/16"
 }
 
+# Default subnet cid block
 variable "aws_subnet_cidr_block" {
   "default" = "10.0.1.0/24"
 }
 
+# Default key pair name
 variable "aws_key_name" {
   "default" = "pm_keypair"
 }
 
+# Template provider
 provider "aws" {
   region = "${var.aws_region}"
 }
 
+/* Create a key pair for ssh connectivity
+   - pass an existing public key to create key pair */
 resource "aws_key_pair" "pm_key_pair" {
   key_name   = "${var.aws_key_name}"
   public_key = "${file(var.ssh_public_key_path)}"
 }
 
+/* Create the vpc with defined cidr block
+   - provide a matching name tag */
 resource "aws_vpc" "pm_vpc" {
   cidr_block = "${var.aws_vpc_cidr_block}"
 
@@ -50,6 +62,8 @@ resource "aws_vpc" "pm_vpc" {
   }
 }
 
+/* Create an internet gateway for external traffic
+   - provide a matching name tag */
 resource "aws_internet_gateway" "pm_internet_gateway" {
   vpc_id = "${aws_vpc.pm_vpc.id}"
 
@@ -58,12 +72,16 @@ resource "aws_internet_gateway" "pm_internet_gateway" {
   }
 }
 
+# Create a default route for the vpc
 resource "aws_route" "pm_route" {
   route_table_id         = "${aws_vpc.pm_vpc.main_route_table_id}"
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = "${aws_internet_gateway.pm_internet_gateway.id}"
 }
 
+/* Create a vpc subnet with defined cidr block
+   - configure public ip assignment
+   - provide a matching name tag */
 resource "aws_subnet" "pm_subnet" {
   vpc_id                  = "${aws_vpc.pm_vpc.id}"
   cidr_block              = "${var.aws_subnet_cidr_block}"
@@ -74,6 +92,12 @@ resource "aws_subnet" "pm_subnet" {
   }
 }
 
+/* Create a security group for the docker host
+   - inbound 22 from ssh source cidr block
+   - inbound 80 from web source cidr block
+   - inbound 5601 from web source cidr block
+   - output everything
+   - provide a matching name tag */
 resource "aws_security_group" "pm_security_group" {
   name        = "pm_web_security_group"
   description = "Allow SSH and HTTP"
@@ -112,6 +136,16 @@ resource "aws_security_group" "pm_security_group" {
   }
 }
 
+/* Create docker host from amazon linux ami
+   - configure user and key pair
+   - configure instance size and type
+   - configure vpc and subnet
+   - configure userdata for security patching at boot
+   - provide a matching name tag
+   - copy over ansible source
+   - install ansible dependencies
+   - update pip and install ansible
+   - run ansible playbook */
 resource "aws_instance" "pm_docker" {
   connection {
     user = "${aws_instance_connection_user}"
@@ -158,10 +192,12 @@ resource "aws_instance" "pm_docker" {
   }
 }
 
+# Output nginx endpoint
 output "nginx_url" {
   value = "http://${aws_instance.pm_docker.public_ip}/"
 }
 
+# Output kibana endpoint
 output "kibana_url" {
   value = "http://${aws_instance.pm_docker.public_ip}:5601/"
 }
--- variables.tf
+++ variables.tf
@@ -1,15 +1,19 @@
+# Default docker instance type
 variable "docker_instance_type" {
   "default" = "m3.medium"
 }
 
+# Default docker root volume size
 variable "docker_instance_root_block_device_volume_size" {
   "default" = "50"
 }
 
+# Default ssh private key path
 variable "ssh_private_key_path" {
   "default" = "~/.ssh/id_rsa"
 }
 
+# Default ssh public key path
 variable "ssh_public_key_path" {
   "default" = "~/.ssh/id_rsa.pub"
 }
